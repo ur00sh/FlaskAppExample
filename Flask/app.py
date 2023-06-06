f601ff979922b8cf2ea540e95081b9b5923f35b2
Urosh Petrovic <87550140+ur00sh@users.noreply.github.com>
2023-06-06 16:33:26 +0200

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,55 +1,59 @@
-#importovanje modula
+#importovanje potrebnih modula
 from flask import Flask, render_template, request, json
-from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
+from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
+#definisanje aplikacije
+app = Flask(__name__, template_folder='') #uneti direktorijum gde se nalaze templates za flask
+
+#definisanje metode koja vodi na pocetnu stranicu
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+#definisanje metode koja vodi na stranicu za prijavu
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+#definisanje stranice koja se pojavljuje nakon uspesnog unosenja podataka
+
+#definisanje POST metode
+@app.route('/api/signup', methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    
+    if len(data) == 0:
+        conn.commit()
+        return json.dumps({'message':'Korisnik je uspesno prijavljen!'})
+    else:
+        return json.dumps({'error':str(data[0])})
+    
+    #proverava tacnost unetih podataka
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>Sva polja su popunjena dobro!</span>'})
+    else:
+        return json.dumps({'html':'<span>Popunite potrebna polja!</span>'})
+
+#konfiguracija mysql-a
 mysql = MySQL()
-conn = mysql.connect()
-cursor = conn.connect()
-hashed_password = generate_password_hash(password)
-
-# mysql konfiguracija
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '' #uneti sifru od MySql-a
 app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-#pozivanje procedure za kreiranje korisnika
-cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+#ostvarivanje veze sa mysql bazom
+conn = mysql.connect()
+cursor = conn.cursor()
 
+#hash lozinka 
+_hashed_password = generate_password_hash (_password)
+
+cursor.callproc('sp_createUser',(_name, _email, _hashed_password))
 data = cursor.fetchall()
-
-if len(data) is 0:
-    conn.commit()
-    return json.dumps({'message':'User created successfully!'})
-else:
-    return json.dumps({'error':str(data[0])})
-
-@app.route("/")
-#definisanje glavne funkcije
-def main():
-    #renderuje html
-    return render_template('index.html', template_folder = "/home/urosh/Flask/templates")
-
-#stranica za ulogovanje
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')
-
-@app.route('/api/signup', method=['POST'])
-def signUp():
-    #cita upisane podatke iz interfejsa 
-    name = request.form['inputNmae']
-    email = request.form['inputEmail']
-    password = request.form['inputPassword']
-    
-    #potvrdjuje primljenje vrednosti
-    if name and email and password:
-        return json.dumps({'html':'<span>All fields good!</span>'})
-    else:
-        return json.dumps({'html':'<span>All fields false!</span>'})
 
 if __name__ == "__main__":
     app.run()
